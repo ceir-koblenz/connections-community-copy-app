8eca114aaa8ad38a9d12965715674f32c9731fb6
Tim <amazingcraftlp@gmail.com>
2019-09-01 14:09:26 +0200

    Routing für ServiceDocument-Komponente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CommunityComponent } from './components/community/community.component';
+import { ServiceDocumentComponent } from './components/service-document/service-document.component';
 
 const routes: Routes = [
-  { path: 'community-selection/:id', component: CommunityComponent}
+  { path: '', redirectTo: '/service', pathMatch: 'full' },
+  { path: 'community-selection/:id', component: CommunityComponent},
+  { path: 'service', component: ServiceDocumentComponent}
 ];
 
 @NgModule({
