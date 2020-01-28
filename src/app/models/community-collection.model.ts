2b6e06e2022ec6094c1fe205984b88bfb341f47e
Andreas Weber <andweb@live.uni-koblenz.de>
2020-01-28 11:06:33 +0100

    #63 Refactoring Community Komponenten+Service

--- src/app/models/community-collection.model.ts
+++ src/app/models/community-collection.model.ts
@@ -1,7 +1,4 @@
 import { IEntityModel } from './i-entity-model';
-import { CommunityCollectionXmlParser } from '../xml-parser/communitycollection-xml-parser';
-import { ApiClientService } from '../services/api-client/api-client.service';
-import { EntityLink } from '../common/entity-link';
 import { Community } from './community.model';
 
 /**
@@ -14,32 +11,4 @@ import { Community } from './community.model';
 export class CommunityCollection implements IEntityModel {
     shouldCopy: boolean;
     public communities: Array<Community> = new Array<Community>();
-
-    /**
-     * Lädt die CommunityCollection anhand der übergebenen Url von der Api und
-     * befüllt die Objektinstanz.
-     *
-     * @static
-     * @param {ApiClientService} client
-     * @param {EntityLink<CommunityCollection>} link
-     * @returns {Promise<CommunityCollection>}
-     * @memberof CommunityCollection
-     */
-    static async load(client: ApiClientService, link: EntityLink<CommunityCollection>): Promise<CommunityCollection> {
-        var xmlParser = new CommunityCollectionXmlParser()
-        var result = new CommunityCollection();
-
-        var nextPageLink: URL = link.url;
-
-        do {
-            var currentXml = await client.loadXML(nextPageLink)
-            nextPageLink = xmlParser.getNextPageUrl(currentXml)
-            xmlParser.fillFromXml(result, currentXml)  // RemoteApplicationCollection Instanz anhand des XMLs befüllen
-        } while (nextPageLink !== null);
-
-        link.model = result;
-
-        return result;
-    }
-
 }
--- src/app/services/community/community.service.ts
+++ src/app/services/community/community.service.ts
@@ -6,7 +6,16 @@ import { CommunityXmlWriter } from './community-xml-writer';
 import { HttpResponse } from '@angular/common/http';
 import { LoggingService } from '../logging/logging.service';
 import { CommunityXmlParser } from 'src/app/xml-parser/community-xml-parser';
+import { CommunityCollection } from 'src/app/models/community-collection.model';
+import { EntityLink } from 'src/app/common/entity-link';
+import { CommunityCollectionXmlParser } from 'src/app/xml-parser/communitycollection-xml-parser';
 
+/**
+ * Service zum Laden und Erstellen von Communities.
+ *
+ * @export
+ * @class CommunityService
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -14,22 +23,30 @@ export class CommunityService {
   private createPath: string = "/communities/service/atom/communities/my"
 
 
+  /**
+   *Creates an instance of CommunityService.
+   * @param {ApiClientService} apiClient
+   * @param {LoggingService} loggingService
+   * @memberof CommunityService
+   */
   constructor(private apiClient: ApiClientService,
     private loggingService: LoggingService) { }
 
-
   /**
-   * Lädt die Community anhand der übergebenen Url von der Api.
-   * Lädt den XML-String von der Api und parst diesen anschließend.
+   * Lädt die Community anhand der übergebenen Id von der Api
+   * und gibt das Ergebnis zurück.
    *
-   * @static
-   * @param {ApiClientService} client
-   * @param {URL} url
+   * @param {string} commId
    * @returns {Promise<Community>}
-   * @memberof Community
+   * @memberof CommunityService
    */
-  async load(client: ApiClientService, url: URL): Promise<Community> {
-    var xmlString = await client.loadXML(url); // Raw XML laden
+  async loadById(commId: string): Promise<Community> {
+    if (!commId) return null;
+
+    var apiUrl = getConfig().connectionsUrl;
+    var commUrl = new URL(apiUrl + "/communities/service/atom/community/instance?communityUuid=" + commId);
+
+    var xmlString = await this.apiClient.loadXML(commUrl); // Raw XML laden
 
     var xmlParser: CommunityXmlParser = new CommunityXmlParser()
     var result = new Community();
@@ -39,6 +56,30 @@ export class CommunityService {
     return result;
   }
 
+  /**
+ * Lädt die CommunityCollection anhand der übergebenen Url von der Api und
+ * befüllt die Objektinstanz.
+ *
+ * @param {EntityLink<CommunityCollection>} link
+ * @returns {Promise<CommunityCollection>}
+ * @memberof CommunityService
+ */
+  async loadCollection(link: EntityLink<CommunityCollection>): Promise<CommunityCollection> {
+    var xmlParser = new CommunityCollectionXmlParser()
+    var result = new CommunityCollection();
+
+    var nextPageLink: URL = link.url;
+
+    do {
+      var currentXml = await this.apiClient.loadXML(nextPageLink)
+      nextPageLink = xmlParser.getNextPageUrl(currentXml)
+      xmlParser.fillFromXml(result, currentXml)  // RemoteApplicationCollection Instanz anhand des XMLs befüllen
+    } while (nextPageLink !== null);
+
+    link.model = result;
+
+    return result;
+  }
 
   /**
    * Erstellt die übergebene Community und gibt die Httpresponse zurück.
