5c8f7a888376952a2ba4768b2b0a5b791f0d57c9
Andreas Weber <andweb@live.uni-koblenz.de>
2020-01-28 11:26:14 +0100

    #63 Refactoring CommunityWrapper-Komponente

--- src/app/components/app/community-wrapper/community-wrapper.component.ts
+++ src/app/components/app/community-wrapper/community-wrapper.component.ts
@@ -5,12 +5,25 @@ import { CreateTemplateService, CreateTemplateResult } from 'src/app/services/cr
 import { ProcessStatus } from 'src/app/common/process-status';
 import { Location } from '@angular/common';
 
+/**
+ * Wrapper um die Community-Komponente, welche die Funktionen der App (neue Community anlegen..) bereitstellt.
+ *
+ * @export
+ * @class CommunityWrapperComponent
+ * @implements {OnInit}
+ */
 @Component({
   selector: 'app-community-wrapper',
   templateUrl: './community-wrapper.component.html',
   styleUrls: ['./community-wrapper.component.sass']
 })
 export class CommunityWrapperComponent implements OnInit {
+  /**
+   * Die Id der Community, die in der gewrappten Komponente angezeigt werden soll.
+   *
+   * @type {string}
+   * @memberof CommunityWrapperComponent
+   */
   public commId: string = null;
 
   /**
@@ -28,33 +41,63 @@ export class CommunityWrapperComponent implements OnInit {
    * @memberof CommunityWrapperComponent
    */
   public processRunning: boolean = false;
+  /**
+   * Ergebnis des Erstellens eines neuen Commmunity.
+   *
+   * @type {CreateTemplateResult}
+   * @memberof CommunityWrapperComponent
+   */
   public processResult: CreateTemplateResult = null;
+  /**
+   * Hält Informationen zum aktuellen Prozessfortschritt
+   *
+   * @private
+   * @type {ProcessStatus}
+   * @memberof CommunityWrapperComponent
+   */
   private processStatus: ProcessStatus = new ProcessStatus();
 
+  /**
+   *Creates an instance of CommunityWrapperComponent.
+   * @param {ActivatedRoute} route
+   * @param {CreateTemplateService} createService
+   * @param {Location} _location
+   * @memberof CommunityWrapperComponent
+   */
   constructor(private route: ActivatedRoute,
     private createService: CreateTemplateService,
     private _location: Location) {
 
+      // Id der anzuzeigenden Community aus Url auslesen
     this.commId = this.route.snapshot.params.id;
     this.commLoaded = this.commLoaded.bind(this);
   }
 
+  /**
+   * Startet den Prozess des neu Anlegens einer Community.
+   *
+   * @memberof CommunityWrapperComponent
+   */
   async startProcess() {
     this.processRunning = true;
     this.processResult = await this.createService.create(this.community, this.processStatus);
-
     this.processRunning = false;
   }
 
+  /**
+   * Setzt die übergebene Community als Member.
+   *
+   * @param {Community} community
+   * @memberof CommunityWrapperComponent
+   */
   commLoaded(community: Community) {
     this.community = community;
   }
 
-  ngOnInit() {
-  }
-
-  backClicked() {
-    this._location.back();
-  }
-
+  /**
+   * Führt initialisierungslogik durch. 
+   *
+   * @memberof CommunityWrapperComponent
+   */
+  ngOnInit(){}
 }
